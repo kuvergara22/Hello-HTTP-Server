a5712643a6347fe805e5d2db74f973b020952925
User <user@Users-MacBook-Pro-3.local>
2018-12-19 15:10:34 -0800

    firts node project

---
+++ README.md
@@ -0,0 +1,2 @@
+web server that will only respond to a basic request for http://localhost:8080/.
+Your server should also log each request to the consol
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// write your code here
+
+var http = require ('http');
+
+http.createServer(aResponse).listen(8080);
+console.log('server is now running 127.0.0.1')
+
+function aResponse(request, response){
+    console.log('print this' + request.url);
+    response.writeHead(200,{'content-type': 'text/plain'});
+    response.write('Hello World');
+    response.status(200);
+    response.end();
+}
+
+
+
+
+
+
